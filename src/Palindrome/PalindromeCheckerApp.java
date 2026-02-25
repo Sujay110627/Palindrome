196dbb3d62f5f6a844000f1128c5fcb45e6eb136
Sujay110627 <sujay110627@gmail.com>
2026-02-25 09:24:52 +0530

    palindrome

--- src/Palindrome/PalindromeCheckerApp.java
+++ src/Palindrome/PalindromeCheckerApp.java
@@ -1,4 +1,12 @@
 package Palindrome;
 
 public class PalindromeCheckerApp {
+    public static void main(String[] args) {
+
+
+        System.out.println("Welcome to the Palindrome Checker Application!");
+        System.out.println("This application validates whether a given string is a palindrome.");
+
+        System.out.println("Program initialized successfully.");
+    }
 }
