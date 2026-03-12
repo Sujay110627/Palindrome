1d53598cbe0e24b051fda07ac50e638f333fb3a9
SUJAY KUMAR D S <sujay110627@gmail.com>
2026-03-12 09:14:08 +0530

    Refactor PalindromeCheckerApp to Use Stack and Queue

--- src/Palindrome/PalindromeCheckerApp.java
+++ src/Palindrome/PalindromeCheckerApp.java
@@ -1,12 +1,49 @@
-package Palindrome;
+import java.util.Scanner;
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
 
-public class PalindromeCheckerApp {
+public class UseCase6PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to the Palindrome Checker Application!");
-        System.out.println("This application validates whether a given string is a palindrome.");
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-        System.out.println("Program initialized successfully.");
+        // Create Stack and Queue
+        Stack<Character> stack = new Stack<>();
+        Queue<Character> queue = new LinkedList<>();
+
+        // Push to stack and enqueue to queue
+        for (int i = 0; i < input.length(); i++) {
+            char ch = input.charAt(i);
+            stack.push(ch);      // LIFO
+            queue.add(ch);       // FIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue from queue and pop from stack
+        for (int i = 0; i < input.length(); i++) {
+            char stackChar = stack.pop();
+            char queueChar = queue.remove();
+
+            if (stackChar != queueChar) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        // Display result
+        if (isPalindrome) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
