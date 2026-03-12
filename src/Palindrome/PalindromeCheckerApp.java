87e729d362dda15d9fe07f52db750d0504d42b3c
SUJAY KUMAR D S <sujay110627@gmail.com>
2026-03-12 09:28:05 +0530

    Implement palindrome checking functionality

--- src/Palindrome/PalindromeCheckerApp.java
+++ src/Palindrome/PalindromeCheckerApp.java
@@ -1,12 +1,48 @@
-package Palindrome;
+import java.util.Scanner;
 
-public class PalindromeCheckerApp {
+class PalindromeChecker {
+
+    // Method to check palindrome
+    public boolean checkPalindrome(String str) {
+
+        int start = 0;
+        int end = str.length() - 1;
+
+        while (start < end) {
+            if (str.charAt(start) != str.charAt(end)) {
+                return false;
+            }
+            start++;
+            end--;
+        }
+
+        return true;
+    }
+}
+
+public class UseCase11PalindromeCheckerApp {
+
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
 
-        System.out.println("Welcome to the Palindrome Checker Application!");
-        System.out.println("This application validates whether a given string is a palindrome.");
+        // Take input from user
+        System.out.print("Enter a string: ");
+        String input = sc.nextLine();
 
-        System.out.println("Program initialized successfully.");
+        // Create object of PalindromeChecker
+        PalindromeChecker checker = new PalindromeChecker();
+
+        // Call method
+        boolean result = checker.checkPalindrome(input);
+
+        // Display result
+        if (result) {
+            System.out.println("The given string is a Palindrome.");
+        } else {
+            System.out.println("The given string is NOT a Palindrome.");
+        }
+
+        sc.close();
     }
 }
